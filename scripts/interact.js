2e1644756361f84fb16fea2d62870375428a3f84
Sami Belhaj <sami.belhadj@gmail.com>
2025-03-16 15:10:35 +0100

    Update interact.js

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,21 +1,42 @@
 require('dotenv').config();
 const Web3 = require('web3');
 
-const web3 = new Web3("https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID");
-const contractABI = [ /* Copier ABI de Remix */ ];
-const contractAddress = "YOUR_CONTRACT_ADDRESS";
+const web3 = new Web3(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+const contractABI = [ /* Copier ABI de Remix après compilation */ ];
+const contractAddress = process.env.CONTRACT_ADDRESS;
 
 const contract = new web3.eth.Contract(contractABI, contractAddress);
+const account = web3.eth.accounts.privateKeyToAccount(process.env.ACCOUNT_PRIVATE_KEY);
+web3.eth.accounts.wallet.add(account);
 
 async function joinLottery() {
-    const accounts = await web3.eth.getAccounts();
-    await contract.methods.joinLottery().send({ from: accounts[0], value: web3.utils.toWei("0.01", "ether") });
-    console.log("Vous avez rejoint la lottery !");
+    const tx = contract.methods.joinLottery();
+    const gas = await tx.estimateGas({ from: account.address, value: web3.utils.toWei("0.01", "ether") });
+    const txData = {
+        from: account.address,
+        to: contractAddress,
+        gas,
+        value: web3.utils.toWei("0.01", "ether"),
+        data: tx.encodeABI()
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(txData, account.privateKey);
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    console.log("Transaction hash:", receipt.transactionHash);
 }
 
 async function pickWinner() {
-    const accounts = await web3.eth.getAccounts();
-    await contract.methods.pickWinner().send({ from: accounts[0] });
+    const tx = contract.methods.pickWinner();
+    const gas = await tx.estimateGas({ from: account.address });
+    const txData = {
+        from: account.address,
+        to: contractAddress,
+        gas,
+        data: tx.encodeABI()
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(txData, account.privateKey);
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     console.log("Le gagnant a été sélectionné !");
 }
 
